ec2409027cb8ab9f29339003c04f8d19e225bf9c
Richard Adule <big.richi@outlook.com>
2021-01-24 03:05:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
       },
     :names=> {
       :reading_railroad=> {
-        :mortgage_vlaue=> "100"
+        :mortgage_vlaue=> 100
       },
       :pennsylvania_railroad=> {
         :mortgage_vlaue=> 200
